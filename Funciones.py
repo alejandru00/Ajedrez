9bad156ca5f128f9c41a903ddddf76489672e128
alejandru00 <alejandrolliber2004@gmail.com>
2022-12-12 23:55:43 +0100

    commit

--- Funciones.py
+++ Funciones.py
@@ -1,8 +1,8 @@
 from Datos import *
 
-def imprimir_tablero(tablero):
-    print("    A B C D E F G H")
-    print("   ――—―—―—―—―—―—―—―—")
+def imprimir_tablero(tablero):          #printeo el tablero con el nombre de las filas y columnas.
+    print("    A B C D E F G H")        
+    print("  + ――—―—―—―—―—―—―—―—")
     
     for i in range(8): 
         print(i+1, "|", end = " ")
@@ -11,42 +11,163 @@ def imprimir_tablero(tablero):
         print()
     print("\n")
 
+
 def mover_pieza():
-    if input == ("A", "B", "C", "D", "E", "F", "G", "H"):
-        columna_inicio = (0, 1, 2, 3, 4, 5, 6, 7)
+    guardar_archivo = input("Esta partida va a guardarse en un archivo. ¿Cómo quieres que se llame este? ")      #Pregunto el nombre el archivo donde voy a guardar las jugadas.
 
-    else:
-        print("No es una columna válida. Vuelve a intentarlo. \n")
-        return mover_pieza
+    if negras[4] or blancas[4] in tablero:                                           #Mientras el rey de cada bando esté en el tablero, el juego sigue.
+        
+        f = open(guardar_archivo, "w")                                                  #Creo el archivo donde voy a guardar las jugadas.
 
-    fila_inicio = int(input("Introduce la fila de la pieza a mover: "))
-    columna_inicio = int(input("Introduce la columna de la pieza a mover: "))
+        while True:
+            fila_inicio = int(input("Introduce la fila de la pieza a mover: "))         #Pido la fila y columna de la pieza a mover
+            if fila_inicio == 1:                                                        #... y hago mas intuitivo el input para el jugador
+                fila_inicio = 0                                                         #... ya que en el tablero las filas empiezan en 0 y no en 1.
+                break
+            elif fila_inicio == 2:
+                fila_inicio = 1
+                break
+            elif fila_inicio == 3:
+                fila_inicio = 2
+                break
+            elif fila_inicio == 4:
+                fila_inicio = 3
+                break
+            elif fila_inicio == 5:
+                fila_inicio = 4
+                break
+            elif fila_inicio == 6:
+                fila_inicio = 5
+                break
+            elif fila_inicio == 7:
+                fila_inicio = 6
+                break
+            elif fila_inicio == 8:
+                fila_inicio = 7
+                break
+            else:
+                print("Esa fila no existe, pruebe otra vez.")
 
-    if tablero[fila_inicio][columna_inicio] in negras:
-        print("Es una pieza negra. \n")
-    elif tablero[fila_inicio][columna_inicio] in blancas:
-        print("Es una pieza blanca. \n")
-    else:
-        print("No hay ninguna pieza en esa posición. Vuelve a intentarlo. \n")
-        return mover_pieza
+        while True:
+            columna_inicio = input("Introduce la columna de la pieza a mover: ")        #Pido la columna de la pieza a mover
+            if columna_inicio == "A":                                                   #... y hago mas intuitivo el input para el jugador
+                columna_inicio = 0                                                      #... ya que en el tablero las columnas empiezan en 0 y no en A.
+                break
+            elif columna_inicio == "B":
+                columna_inicio = 1
+                break
+            elif columna_inicio == "C":
+                columna_inicio = 2
+                break
+            elif columna_inicio == "D":
+                columna_inicio = 3
+                break
+            elif columna_inicio == "E":
+                columna_inicio = 4
+                break
+            elif columna_inicio == "F":
+                columna_inicio = 5
+                break
+            elif columna_inicio == "G":
+                columna_inicio = 6
+                break
+            elif columna_inicio == "H":
+                columna_inicio = 7
+                break
+            else:
+                print("Esa columna no existe, pruebe otra vez.")
+                
 
-
-    fila_final = int(input("Introduce la fila de destino: "))
-    columna_final = int(input("Introduce la columna de destino: "))
-
-    if tablero[fila_final][columna_final] in tablero:
-        print("Hay una pieza en esa posición. \n")
-        if tablero[fila_final][columna_final] in negras and tablero[fila_inicio][columna_inicio] in negras:
-            print("No puedes comer una pieza de tu mismo color. \n")
-            return mover_pieza
+        if tablero[fila_inicio][columna_inicio] in negras:                     #Comunico al jugador que tipo de pieza que ha seleccionado.
+            print("Es una pieza negra. \n")
+        elif tablero[fila_inicio][columna_inicio] in blancas:
+            print("Es una pieza blanca. \n")
         else:
-            print("Has comido una pieza. \n")
+            print("No hay ninguna pieza en esa posición. Vuelve a intentarlo. \n")      #Si no hay ninguna pieza en esa posición, vuelve a pedir la posición de la pieza a mover.
+            return mover_pieza()
 
-    else:
-        print("No hay ninguna pieza en esa posición. \n")
+        while True:
+            fila_final = int(input("Introduce la fila de destino: "))           #Pido la fila y columna de destino
+            if fila_final == 1:                                                 #... y hago mas intuitivo el input para el jugador
+                fila_final = 0                                                  #... ya que en el tablero las filas empiezan en 0 y no en 1.
+                break
+            elif fila_final == 2:
+                fila_final = 1
+                break
+            elif fila_final == 3:
+                fila_final = 2
+                break
+            elif fila_final == 4:
+                fila_final = 3
+                break
+            elif fila_final == 5:
+                fila_final = 4
+                break
+            elif fila_final == 6:
+                fila_final = 5
+                break
+            elif fila_final == 7:
+                fila_final = 6
+                break
+            elif fila_final == 8:
+                fila_final = 7
+                break
+            else:
+                print("Esa fila no existe, pruebe otra vez.")
+                
 
-     
-    tablero[fila_final][columna_final] = tablero[fila_inicio][columna_inicio] 
-    tablero[fila_inicio][columna_inicio] = " "
-    print(imprimir_tablero(tablero))
+        while True:
+            columna_final = input("Introduce la columna de destino: ")        #Pido la columna de destino
+            if columna_final == "A":                                          #... y hago mas intuitivo el input para el jugador              
+                columna_final = 0                                             #... ya que en el tablero las columnas empiezan en 0 y no en A.
+                break
+            elif columna_final == "B":
+                columna_final = 1
+                break
+            elif columna_final == "C":
+                columna_final = 2
+                break
+            elif columna_final == "D":
+                columna_final = 3
+                break
+            elif columna_final == "E":
+                columna_final = 4
+                break
+            elif columna_final == "F":
+                columna_final = 5
+                break
+            elif columna_final == "G":
+                columna_final = 6
+                break
+            elif columna_final == "H":
+                columna_final = 7
+                break
+            else:
+                print("Esa columna no existe, pruebe otra vez.")
+
+        if tablero[fila_final][columna_final] in tablero:                       #Compruebo si hay una pieza en la posición de destino.
+            print("Hay una pieza en esa posición. \n")
+            if tablero[fila_final][columna_final] in negras and tablero[fila_inicio][columna_inicio] in negras:
+                print("No puedes comer una pieza de tu mismo color. \n")        #Y en el caso de que sea del mismo color que la pieza que se quiere mover, 
+                return mover_pieza                                              #... no permito el movimiento y hago repetirlo.
+            else:
+                print("Has comido una pieza. \n")                               #En el caso de que sea del color contrario, permito el movimiento.
+
+        else:
+            print("No hay ninguna pieza en esa posición. \n")
+
+        
+        tablero[fila_final][columna_final] = tablero[fila_inicio][columna_inicio]   #Muevo la pieza de la posición de inicio a la de destino.
+        tablero[fila_inicio][columna_inicio] = " "                                  #... y vacío la posición de inicio.
+        print(imprimir_tablero(tablero))                                            #Imprimo el tablero con la pieza movida.                  
+
+    
+        f.write(f"{imprimir_tablero(tablero)}")                                     #Guardo el movimiento (nuevo tablero) en el archivo de texto.                 
+        f.close()
+
+    else: 
+        pass
+
+
+
 
--- Tablero.py
+++ Tablero.py
@@ -4,9 +4,7 @@ from Funciones import *
 
 def jugar():
     imprimir_tablero(tablero)
-    while negras[4] or blancas[4] in tablero:
-        mover_pieza()
-        break
+    mover_pieza()
 
     print("Se ha terminado el juego:")
     if not negras[4] in tablero:
@@ -14,4 +12,16 @@ def jugar():
     elif not blancas[4] in tablero:
         print("¡Ganan las negras!")
 
+def volver_a_jugar():
+    while True:
+        respuesta = input("¿Quieres volver a jugar? (si/no) ")
+        if respuesta == "si":
+            print("¡Vamos a jugar otra vez! \n")
+            return jugar()
+        elif respuesta == "no":
+            return False
+        else:
+            print("No has introducido una opción válida. Prueba otra vez.")
+
 jugar()
+volver_a_jugar()

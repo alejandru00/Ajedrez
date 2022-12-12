9ad473c231ba6d8babecda2f3c441d193ea8eadc
alejandru00 <alejandrolliber2004@gmail.com>
2022-12-12 20:03:43 +0100

    commit

--- Tablero.py
+++ Tablero.py
@@ -1,62 +1,17 @@
+from Datos import *
+from Funciones import *
 
 
-tablero = [['♜',	'♞',	'♝',	'♛',   '♚', 	'♝', 	'♞', 	'♜'],
-           ['♟',	'♟',	'♟',	'♟',   '♟', 	'♟', 	'♟', 	'♟'],
-           [' ',	 ' ',     ' ',     ' ',    ' ', 	 ' ', 	 ' ', 	  ' '],
-           [' ',	 ' ',	  ' ',	   ' ',    ' ', 	 ' ', 	 ' ', 	  ' '],
-           [' ',	 ' ',	  ' ',	   ' ',    ' ', 	 ' ', 	 ' ', 	  ' '],
-           [' ',	 ' ',	  ' ',	   ' ',    ' ', 	 ' ', 	 ' ', 	  ' '],
-           ['♙',	'♙',	'♙',	'♙',   '♙', 	'♙', 	'♙', 	'♙'],
-           ['♖',	'♘',	'♗',	'♕',   '♔', 	'♗', 	'♘', 	'♖']
-          ]
+def jugar():
+    imprimir_tablero(tablero)
+    while negras[4] or blancas[4] in tablero:
+        mover_pieza()
+        break
 
+    print("Se ha terminado el juego:")
+    if not negras[4] in tablero:
+        print("¡Ganan las blancas!")
+    elif not blancas[4] in tablero:
+        print("¡Ganan las negras!")
 
-negras = ['♜', '♞', '♝', '♛', '♚', '♝',	'♞', '♜', '♟', '♟',	'♟', '♟', '♟', '♟', '♟', '♟']
-blancas = ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖', '♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙']
-
-def imprimir_tablero(tablero):
-    print("    A B C D E F G H")
-    print("  + ――—―—―—―—―—―—―—―—")
-    
-    for i in range(8): 
-        print(i+1, "|", end = " ")
-        for j in range(8):
-            print(tablero[i][j], end = " ")
-        print()
-    print("\n")
-        
-imprimir_tablero(tablero)
-
-
-def quieres_jugar():
-    respuesta = input("¿Quieres jugar? (si/no): ")
-    if respuesta == "si":
-        print("Dale pa, juega.")
-    elif respuesta == "no":
-        print("Se acabó el juego. :(")
-        print("Este es tu tablero: \n")
-        print(imprimir_tablero(tablero))
-
-quieres_jugar()
-
-def movimientos():
-    print("¿Qué pieza quieres mover?")
-    pieza = input()
-    if pieza in negras:
-        print("Es una pieza negra. \n")
-    elif pieza in blancas:
-        print("Es una pieza blanca. \n")
-    else:
-        print("No hay ninguna pieza en esa posición. \n")
-
-movimientos()
-
-def mover_pieza():
-    print("¿A qué posición quieres mover la pieza?")
-    posicion = input()
-    if posicion in tablero:
-        print("Hay una pieza en esa posición. \n")
-    else:
-        print("No hay ninguna pieza en esa posición. \n")
-
-mover_pieza()
+jugar()
